02efa05404c04a8ceb926f4788cb14016d515ea8
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2023-12-25 12:02:25 -0300

    ScreenMatch push 2

--- EstudosSpring/screenMatch/src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
+++ EstudosSpring/screenMatch/src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenMatch;
 
+import br.com.alura.screenMatch.model.DadosSerie;
 import br.com.alura.screenMatch.service.ConsumoApi;
+import br.com.alura.screenMatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,8 +20,12 @@ public class ScreenMatchApplication implements CommandLineRunner
 	public void run(String... args) throws Exception
 	{
 		var consumoApi = new ConsumoApi();
+		ConverteDados conversor = new ConverteDados();
 
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=stranger+things+&y=2019&apikey=18ae168a");
 		System.out.println(json);
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
